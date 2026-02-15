8cbb18bd094c4a97ee88b72b80b3ede93fa4e898
premium00495 <premium00495@gmail.com>
2026-02-15 18:27:08 +0500

    Update stream URLs to new CDN links

--- api/streams.js
+++ api/streams.js
@@ -1,11 +1,11 @@
 export default function handler(req, res) {
   res.status(200).json({
     urls: {
-      "240p": "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-      "360p": "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-      "480p": "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-      "720p": "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-      "1080p": "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+      "240p": "https://tencentcdn8.tamashaweb.com/v1/019bf00087161567ee93346674d025/019bffb7d77215fc600b1b67f81952/tmsh_srt_output_clone_240p.m3u8",
+      "360p": "https://tencentcdn8.tamashaweb.com/v1/019bf00087161567ee93346674d025/019bffb7d77215fc600b1b67f81952/tmsh_srt_output_clone_360p.m3u8",
+      "480p": "https://tencentcdn8.tamashaweb.com/v1/019bf00087161567ee93346674d025/019bffb7d77215fc600b1b67f81952/tmsh_srt_output_clone_480p.m3u8",
+      "720p": "https://tencentcdn8.tamashaweb.com/v1/019bf00087161567ee93346674d025/019bffb7d77215fc600b1b67f81952/tmsh_srt_output_clone_720p.m3u8",
+      "1080p": "https://tencentcdn8.tamashaweb.com/v1/019bf00087161567ee93346674d025/019bffb7d77215fc600b1b67f81952/tmsh_srt_output_clone_1080p.m3u8"
     }
   });
 }
