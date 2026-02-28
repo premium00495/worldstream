59eef84920429be090d97f0396ec9670275c6b38
premium00495 <premium00495@gmail.com>
2026-02-28 21:25:10 +0500

    Update streams.js

--- api/streams.js
+++ api/streams.js
@@ -1,11 +1,11 @@
 export default function handler(req, res) {
   res.status(200).json({
     urls: {
-      "240p": "https://tencentcdn5.tamashaweb.com/v1/0196159eeff41eb4611d121c76c781/0196159f51bb1ea5064913eb2a83ea/TMSHU1WEB_240p.m3u8",
-      "360p": "https://tencentcdn5.tamashaweb.com/v1/0196159eeff41eb4611d121c76c781/0196159f51bb1ea5064913eb2a83ea/TMSHU1WEB_360p.m3u8",
-      "480p": "https://tencentcdn5.tamashaweb.com/v1/0196159eeff41eb4611d121c76c781/0196159f51bb1ea5064913eb2a83ea/TMSHU1WEB_480p.m3u8",
-      "720p": "https://tencentcdn5.tamashaweb.com/v1/0196159eeff41eb4611d121c76c781/0196159f51bb1ea5064913eb2a83ea/TMSHU1WEB_720p.m3u8",
-      "1080p": "https://tencentcdn5.tamashaweb.com/v1/0196159eeff41eb4611d121c76c781/0196159f51bb1ea5064913eb2a83ea/TMSHU1WEB_1080p.m3u8"
+      "240p": "https://tencentcdn11.tamashaweb.com/v1/019bf9b9eb3215fc600b35c80a5fba/019bffdc5fb61ea540a817848da787/TMSHU1WEB_clone_240p.m3u8",
+      "360p": "https://tencentcdn11.tamashaweb.com/v1/019bf9b9eb3215fc600b35c80a5fba/019bffdc5fb61ea540a817848da787/TMSHU1WEB_clone_360p.m3u8",
+      "480p": "https://tencentcdn11.tamashaweb.com/v1/019bf9b9eb3215fc600b35c80a5fba/019bffdc5fb61ea540a817848da787/TMSHU1WEB_clone_480p.m3u8",
+      "720p": "https://tencentcdn11.tamashaweb.com/v1/019bf9b9eb3215fc600b35c80a5fba/019bffdc5fb61ea540a817848da787/TMSHU1WEB_clone_720p.m3u8",
+      "1080p": "https://tencentcdn11.tamashaweb.com/v1/019bf9b9eb3215fc600b35c80a5fba/019bffdc5fb61ea540a817848da787/TMSHU1WEB_clone_1080p.m3u8"
     }
   });
 }
